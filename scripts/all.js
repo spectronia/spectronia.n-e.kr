ff1f3e8d8ea28059f3a22d597b2a015ad616b38b
spectronia <126040678+spectronia@users.noreply.github.com>
2023-02-22 17:24:45 +0900

    Update all.js

--- scripts/all.js
+++ scripts/all.js
@@ -1,2 +1,1 @@
 document.querySelector("header > img").addEventListener("click", () => {location.href = "/"})
-document.querySelector("header > a#2").addEventListener("click", () => {location.href = "https://discord.gg/ucE6SRVDaR"})
